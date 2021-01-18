8e5b96382b4a5c0ce49984a75684b3f44b4515ba
abhishekgedelacode <gabhishekji143@gmail.com>
2021-01-18 19:10:10 +0530

    created lab folder and typed first lab prgm distance between two points

---
+++ lab/dist.py
@@ -0,0 +1,10 @@
+x1 = int(input("enter x1 value: "))
+y1 = int(input("enter y1 value: ")) 
+x2 = int(input("enter x2 value: "))
+y2 = int(input("enter y2 value: "))
+
+d = ((x2-x1)**2 + (y2-y1)**2)**(0.5)
+
+print("the distance between two points: ", d)
+
+
